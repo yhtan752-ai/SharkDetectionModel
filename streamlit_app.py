1559b4eb56d8e75245cd0e1759f35d5eb239c678
yhtan752-ai <yhtan752@gmail.com>
2026-06-17 12:31:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 
 # Point this to your lecturer's GPU server public IP address
 # Inside your local streamlit_app.py or your deployed file:
-GPU_SERVER_URL = "http://152.226.255.18:8000/process-video/"
+GPU_SERVER_URL = "http://152.226.255.18/process-video/"
 
 st.title("🦈 Automated Real-Time Shark Detection & Counter")
 st.write("Upload a video stream below. Processing is offloaded to a high-performance external GPU cluster.")
